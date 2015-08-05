7d42fd1c5fa1ce9b26fc3a1b83e1cbb728eff15b
= <=>
2015-08-05 23:11:05 +0800

    update

--- torch_practice/cnn_forward_cpp/net_info.lua
+++ torch_practice/cnn_forward_cpp/net_info.lua
@@ -7,20 +7,65 @@ require 'image'
 --load the torch result file
 net = torch.load('train.net')
 
+infofile=io.open('info.txt', 'w')
+assert(infofile)
+
 --print(net)
-print(#net.modules)
+
 for i=1,#net.modules do
+	infofile:write(i,' ')
 	print('-------------  ' .. i .. '  -------------')
 	print(net.modules[i])
+	-- SpatialConvolution 	11
+	-- ReLU 		12
+	-- SpatialMaxPooling 	13
+	-- Reshape		14
+	-- Linear		15
+	-- LogSoftMax		16
+	str = tostring(net.modules[i])
+	if string.find(str, 'SpatialConvolution') then
+		infofile:write(11,' ')
+	elseif string.find(str, 'ReLU') then
+		infofile:write(12,' ')
+	elseif string.find(str, 'MaxPooling') then
+		infofile:write(13,' ')
+	elseif string.find(str, 'Reshape') then
+		infofile:write(14,' ')
+	elseif string.find(str, 'Linear') then
+		infofile:write(15,' ')
+	elseif string.find(str, 'LogSoftMax') then
+		infofile:write(16,' ')
+	end
 	print('input:')
 	print(#net.modules[i].gradInput) 
+	infofile:write(net.modules[i].gradInput:nDimension(),' ')
+	for j=1,net.modules[i].gradInput:nDimension() do
+		infofile:write(net.modules[i].gradInput:size()[j],' ')
+	end
 	if net.modules[i].weight ~= nil then
 		print('weight:')
 		print(#net.modules[i].weight) 
+		infofile:write(net.modules[i].weight:nDimension(),' ')
+		for j=1,net.modules[i].weight:nDimension() do
+			infofile:write(net.modules[i].weight:size()[j],' ')
+		end
 		print('bias:')
 		print(#net.modules[i].bias) 
+		infofile:write(net.modules[i].bias:nDimension(),' ')
+		for j=1,net.modules[i].bias:nDimension() do
+			infofile:write(net.modules[i].bias:size()[j],' ')
+		end
+	else
+		infofile:write(0,' ')
 	end
 	print('output:')
 	print(#net.modules[i].output) 
+	infofile:write(net.modules[i].output:nDimension(),' ')
+	for j=1,net.modules[i].output:nDimension() do
+		infofile:write(net.modules[i].output:size()[j],' ')
+	end
+	infofile:write('\n')
 end
+
+infofile:close()
 
