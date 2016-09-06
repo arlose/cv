93e2dfae8f1b6fa8dc019608997dcd2c0c3e95cf
arlose <arlose.fj@gmail.com>
2016-09-06 11:29:32 +0800

    voc2007 dataset generation

---
+++ python_practice/voc2007gen.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+import os
+import string
+import shutil
+import cv2
+import string
+import xml.dom
+import multiprocessing	
+
+suffix_list=['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG']
+
+VOC2007Path = './VOC2007'
+AnnotationsPath = VOC2007Path+'/Annotations'
+ImageSetsPath = VOC2007Path+'/ImageSets'
+JPEGImagesPath = VOC2007Path+'/JPEGImages'
+
+ORGImagePath = './Images'
+ORGLabelPath = './Labels'
+# 生成图像xml文件
+def readrects(labelfile):
+  try:
+    file_object = open(labelfile, 'r')
+    list_of_all_the_lines = file_object.readlines( )
+    num = string.atoi(list_of_all_the_lines[0])
+    rects = []
+    for i in range(1,num+1) :
+      rect = list_of_all_the_lines[i].split()
+      rects.append(rect)
+    file_object.close()
+    return rects
+  except IOError:
+    return None
+
+def create_element(doc,tag,attr):
+  #创建一个元素节点
+  elementNode=doc.createElement(tag)
+  #创建一个文本节点
+  textNode=doc.createTextNode(attr)
+  #将文本节点作为元素节点的子节点
+  elementNode.appendChild(textNode)
+  return elementNode
+
+def genxml(filename, width, height, rects):
+  dom1=xml.dom.getDOMImplementation()#创建文档对象，文档对象用于创建各种节点。
+  doc=dom1.createDocument(None,"annotation",None)
+  top_element = doc.documentElement# 得到根节点
+
+  #folder and filename
+  folderNode=create_element(doc,'folder','MIDDLE')
+  filenameNode=create_element(doc,'filename',filename+'.jpg')
+  top_element.appendChild(folderNode)
+  top_element.appendChild(filenameNode)
+
+  #source
+  sourceNode=doc.createElement('source')
+  databaseNode=create_element(doc,'database','Middle Database')
+  annoNode=create_element(doc,'annotation','Middle')
+  imageNode=create_element(doc,'image','flickr')
+  sourceNode.appendChild(databaseNode)
+  sourceNode.appendChild(annoNode)
+  sourceNode.appendChild(imageNode)
+  top_element.appendChild(sourceNode)
+
+  #size
+  sizeNode=doc.createElement('size')
+  widthNode=create_element(doc,'width',str(width))
+  heightNode=create_element(doc,'height',str(height))
+  depthNode=create_element(doc,'depth','3')
+  sizeNode.appendChild(widthNode)
+  sizeNode.appendChild(heightNode)
+  sizeNode.appendChild(depthNode)
+  top_element.appendChild(sizeNode)
+
+  #segmented
+  segmentedNode=create_element(doc,'segmented','0')
+  top_element.appendChild(segmentedNode)
+  
+  if rects is None:
+    return
+  else:
+    for rect in rects:
+      #object
+      objNode=doc.createElement('object')
+      nameNode=create_element(doc,'name',rect[4])
+      poseNode=create_element(doc,'pose','Unspecified')
+      truncatedNode=create_element(doc,'truncated','0')
+      difficultNode=create_element(doc,'difficult','0')
+      bndboxNode=doc.createElement('bndbox')
+      xminNode=create_element(doc,'xmin',rect[0])
+      yminNode=create_element(doc,'ymin',rect[1])
+      xmaxNode=create_element(doc,'xmax',rect[2])
+      ymaxNode=create_element(doc,'ymax',rect[3])
+      bndboxNode.appendChild(xminNode)
+      bndboxNode.appendChild(yminNode)
+      bndboxNode.appendChild(xmaxNode)
+      bndboxNode.appendChild(ymaxNode)
+      objNode.appendChild(nameNode)
+      objNode.appendChild(poseNode)
+      objNode.appendChild(truncatedNode)
+      objNode.appendChild(difficultNode)
+      objNode.appendChild(bndboxNode)
+      top_element.appendChild(objNode)
+
+  xmlfile=open(AnnotationsPath+'/'+filename+'.xml','w')
+  doc.writexml(xmlfile,addindent=' '*4, newl='\n', encoding='utf-8')
+  xmlfile.close()
+
+# 创建VOC2007系列文件夹
+if not os.path.isdir(VOC2007Path):
+  os.mkdir(VOC2007Path)
+if not os.path.isdir(AnnotationsPath):
+  os.mkdir(AnnotationsPath)
+if not os.path.isdir(ImageSetsPath):
+  os.mkdir(ImageSetsPath)
+if not os.path.isdir(ImageSetsPath+'/Main'):
+  os.mkdir(ImageSetsPath+'/Main')
+if not os.path.isdir(JPEGImagesPath):
+  os.mkdir(JPEGImagesPath)
+
+# 将jpg/txt文件名加middle和side前缀移动到VOC2007文件夹中
+prestrs = []
+dirs = os.listdir(ORGImagePath) 
+for d in dirs:
+  prestrs.append(d)
+
+print prestrs
+
+for prestr in prestrs:
+  for fpathe,dirs,fs in os.walk(ORGImagePath+'/'+prestr):
+    for f in fs:
+      if os.path.splitext(f)[1] in suffix_list:
+        imagefile = os.path.join(fpathe,f)
+        tmpname = imagefile.rsplit('.', 1)
+        txtfile = tmpname[0]+'.txt'
+        txtfile = txtfile.replace('Images','Labels')
+        filename = os.path.basename(imagefile).split('.')[-2]
+        suffix = os.path.basename(imagefile).split('.')[-1]
+        newfilename = prestr+'_'+filename
+        # 如果存在txt标注文件，则移动，否则不移动
+        if(os.path.exists(txtfile)):
+          print filename
+          shutil.move(imagefile, JPEGImagesPath+'/'+newfilename+'.'+suffix)
+          shutil.move(txtfile, AnnotationsPath+'/'+newfilename+'.txt')
+
+# 获取VOC2007文件夹中的图像名称，存入ImageSets/Main/trainval.txt中
+ftxt = open(ImageSetsPath+'/Main/trainval.txt','w')
+for fpathe,dirs,fs in os.walk(JPEGImagesPath):
+  for f in fs:
+    if os.path.splitext(f)[1] in suffix_list:
+      imagefile = os.path.join(fpathe,f)
+      filename = os.path.basename(imagefile).split('.')[-2]
+      ftxt.write(filename+'\n')
+      print filename
+ftxt.close()
+
+# 多进程生成相应的xml文件
+n = 8
+flist = [[] for i in range(n) ]
+ftxt = open(ImageSetsPath+'/Main/trainval.txt','r')
+i=0
+for line in ftxt:
+  flist[i].append(line.strip())
+  i=(i+1)%n
+ftxt.close()
+
+def multigenxml(files):
+  for f in files:
+    f = f+'.jpg'
+    imagefile = os.path.join(JPEGImagesPath,f)
+    labelfile = imagefile.replace('jpg','txt')
+    labelfile = labelfile.replace('JPEGImages','Annotations')
+    xmlfile = labelfile.replace('txt','xml')
+    if(not os.path.exists(xmlfile)):
+      #print imagefile
+      #print labelfile
+      img = cv2.imread(imagefile,1)
+      rects = readrects(labelfile)
+      filename = os.path.basename(imagefile).split('.')[-2]
+      #print filename
+      (height,width,_) = img.shape
+      genxml(filename, width, height, rects)
+
+p = []
+for i in range(n):
+  p.append(multiprocessing.Process(target = multigenxml, args = (flist[i],)))
+for i in range(n):
+  p[i].start()
