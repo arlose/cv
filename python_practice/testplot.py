74b2ce733c487997ca916b7eafd1487d725a1253
arlose <arlose.fj@gmail.com>
2016-12-01 08:12:57 +0800

    matplot save image

---
+++ python_practice/testplot.py
@@ -0,0 +1,15 @@
+# use matplotlib save plot image
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+
+from matplotlib.pyplot import plot,savefig
+
+x=np.linspace(-4,4,30)
+y=np.sin(x);
+
+plot(x,y,'--*b')
+
+savefig('./MyFig.jpg')
